4824f1c64a1efcd604db7876e13e7357d2b20f6e
DanielDlc <dlc.engcomputacao@gmail.com>
2024-05-13 08:43:42 +0100

    name generator

--- 01_Beginner/01_print.py
+++ 01_Beginner/01_print.py
@@ -1,1 +1,6 @@
-print('Wello World!')
+print('Welcome to the band name generator')
+
+city = input('which city did you grow up in\n ')
+pet = input('What is the name of a pet?\n ')
+
+print(f'Your Band name cold be {city} {pet}')
